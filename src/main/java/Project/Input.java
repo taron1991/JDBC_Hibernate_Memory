f23f2d6cb1d86f715ea1463c78ddea0afab5cf73
taron1991 <84153270+taron1991@users.noreply.github.com>
2022-03-21 13:53:38 +0300

    Delete src directory

--- src/main/java/Project/Input.java
+++
@@ -1,7 +0,0 @@
-package Project;
-
-public interface Input {
-    public String askStr(String question);
-
-    public long askLong(String question);
-}
--- src/main/java/Project/UserAction.java
+++
@@ -1,6 +0,0 @@
-package Project;
-
-public interface UserAction {
-    String name(); /*- чисто выводить какой пункт меню выбрал пользак*/
-    boolean execute(Input input, Store memTracker);
-}
